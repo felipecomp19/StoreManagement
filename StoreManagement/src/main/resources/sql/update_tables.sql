1fac714dddbbe1ad6bd4fa832daba589884142d9
Felipe Teixeira <felipecomp19@gmail.com>
2014-03-19 16:56:50 -0300

    release 1.1

--- StoreManagement/src/main/resources/sql/update_tables.sql
+++ StoreManagement/src/main/resources/sql/update_tables.sql
@@ -2,6 +2,9 @@
 --INSERT INTO db_storeManager_test.tb_user_roles (`role`) VALUES ('ROLE_USER');
 
 INSERT INTO tb_user_roles (`role`,`isEnabled`) VALUES ('ROLE_ADMIN', true);
+INSERT INTO tb_user_roles (`role`,`isEnabled`) VALUES ('ROLE_ADMIN', true);
+INSERT INTO tb_user_roles (`role`,`isEnabled`) VALUES ('ROLE_MANAGER', true);
 INSERT INTO tb_user_roles (`role`) VALUES ('ROLE_USER');
 INSERT INTO tb_user_roles (`role`) VALUES ('ROLE_MANAGER');
-INSERT INTO tb_users (`isActive`, `password`, `userName`, `userRole`, `name`) VALUES (true,"$2a$10$KEA5eHefNnUuDjNaHs9mhuP5nK1gG6wjei1mXmxVXLiyWt.WihxRC","master",3,"Master");
+INSERT INTO tb_users (`isActive`, `password`,`userName`, `userRole`, `name`) VALUES (true,"$2a$10$KEA5eHefNnUuDjNaHs9mhuP5nK1gG6wjei1mXmxVXLiyWt.WihxRC","master",3,"Master");
+
